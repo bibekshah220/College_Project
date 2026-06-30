1463ba26b5d6dd14647ef5bf8e7aa3dd4132aa1a
bibekshah220 <bibekshah425@gmail.com>
2026-06-30 23:47:44 +0545

    feat: implement authentication views and dashboard layout with CSS styling

---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CafeManagementSystem.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, DataType(DataType.Password)]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
